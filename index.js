71f7df9833286f7c2cebd307b6819af8d0293b80
brendanjeffreymcmahon <brendan.mcmahon@bloomerang.com>
2023-07-04 09:52:17 -0400

    add the date

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
-const AWS = require('aws-sdk');
-  const ddb = new AWS.DynamoDB.DocumentClient();
-  
-  exports.handler = async (event, context) => {
-    const name = event.body.name;
-  
-    // delete the record from the databse that has the name passed in on the body
-    const deleteParams = {
-      TableName: "cube-cafe-data",
-      Key: {
-        name: name,
-      }
-    };
-  
-  
-    try {
-      await ddb.delete(deleteParams).promise();
-      return { statusCode: 200, body: 'Game Deleted.' };
-    } catch (err) {
-      console.error(err);
-      return { statusCode: 500, body: 'Failed to connect: ' + JSON.stringify(err) };
-    }
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+  const { name, date } = JSON.parse(event.body); // Parse body content from string to JSON
+
+  const params = {
+    TableName: "cube-cafe-data",
+    Key: {
+      name: name,
+      date: date,
+    },
   };
-  
-  
+
+  try {
+    const data = await documentClient.delete(params).promise();
+    console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+    return { statusCode: 200, body: JSON.stringify("DeleteItem succeeded") }; // Return successful response
+  } catch (err) {
+    console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+    return { statusCode: 500, body: JSON.stringify("Unable to delete item") }; // Return error response
+  }
+};
